3920b182b11a5cd52a97646434b67423aea0a7cc
Pedro Artur (Motsu) <71746213+Pedro-Artur-RF@users.noreply.github.com>
2023-05-29 11:10:31 -0300

    1st commit
    Html 100% done, Js and Css In progress

---
+++ Atividade - UI - Summary/script.js
@@ -0,0 +1,4 @@
+document.getElementById("skill-reaction").style.backgroundColor = "var(--LightRed)"
+document.getElementById("skill-memory").style.backgroundColor = "var(--)"
+document.getElementById("skill-verbal").style.backgroundColor = "var(--)"
+document.getElementById("skill-visual").style.backgroundColor = "var(--)"
